33c00d830176f15b7e94f994f5a0776dd848b7fc
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-02-18 18:33:12 +0900

    update gem

--- main.rb
+++ main.rb
@@ -1,3 +1,15 @@
 a = 10
 b = 3 * a + 2
 printf("%d %d\n", a, b)
+
+array = [1, 2, 3, 4, 5]
+
+array.each do |i|
+  puts i
+end
+
+hash = { a: 1, b: 2, c: 3 }
+
+hash.each do |k, v|
+  puts "#{k}: #{v}"
+end
